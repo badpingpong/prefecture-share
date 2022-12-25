d3da6cc09caaae7b806f90b3cccb9c6778d9e3f3
badpingpong <akirayo.badpingpong@gmail.com>
2022-12-25 14:46:20 +0900

    make usePrefectureList typesafe

--- src/presentation/hooks/usePrefectureList.ts
+++ src/presentation/hooks/usePrefectureList.ts
@@ -1,15 +1,19 @@
 import useSWR from "swr";
 import axios from "axios";
+import { Prefecture, PrefectureListResponse } from "@domain/models";
+import { PrefectureTranslator } from "@domain/repository";
 
 const fetcher = (url: string) =>
   axios
-    .get(url, {
+    .get<PrefectureListResponse>(url, {
       headers: { "X-API-KEY": process.env.NEXT_PUBLIC_RESAS_API_KEY },
     })
-    .then((res) => res.data);
+    .then((res) => {
+      return new PrefectureTranslator().translatePrefectureList(res.data);
+    });
 
 export const usePrefectureList = () => {
-  const { data, error, isLoading } = useSWR(
+  const { data, error, isLoading } = useSWR<Prefecture[]>(
     `${process.env.NEXT_PUBLIC_RESAS_API_URL}/v1/prefectures`,
     fetcher
   );
